0da88f09692649216570e8cf9196f6fa41007374
trigen <trigen>
2003-09-14 18:33:15 +0000

    added sound.h

---
+++ Fighterz/client/sound.h
@@ -0,0 +1,9 @@
+/* Allegro datafile object indexes, produced by grabber v4.0.0, MinGW32.s */
+/* Datafile: d:\idev\Fighterz\client\project_files\system\base_snd.dat */
+/* Date: Sun Sep 14 18:54:41 2003 */
+/* Do not hand edit! */
+
+#define CONFIRM                          0        /* SAMP */
+#define FAST_CONFIRM                     1        /* SAMP */
+#define SPAWN                            2        /* SAMP */
+
